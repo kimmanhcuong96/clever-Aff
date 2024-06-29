7d22f75674ce84c7f612be6ab4321dc9bdf9e798
cuongkm <kimmanhcuong96@gmail.com>
2024-06-29 15:01:23 +0700

    udate not found page

--- wp-content/themes/kadence/template-parts/content/error-404.php
+++ wp-content/themes/kadence/template-parts/content/error-404.php
@@ -15,17 +15,20 @@ namespace Kadence;
 		<div class="entry-content-wrap">
 
 			<?php
-			do_action( 'kadence_404_before_inner_content' );
+			do_action('kadence_404_before_inner_content');
 
-			get_template_part( 'template-parts/content/page_header' ); ?>
+			// get_template_part('template-parts/content/page_header'); ?>
+			<p style="font-size: xx-large;font-weight: bold;">
+				<?php esc_html_e('Bạn chưa đăng ký khóa học này', 'kadence'); ?>
+			</p>
 			<p>
-				<?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'kadence' ); ?>
+				<?php esc_html_e('Tìm kiếm khóa học khác tại đây', 'kadence'); ?>
 			</p>
 
 			<?php
 			get_search_form();
 
-			do_action( 'kadence_404_after_inner_content' );
+			do_action('kadence_404_after_inner_content');
 			?>
 		</div>
 	</div><!-- .page-content -->
